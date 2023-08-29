a4cf8cbe417c31d4970a6bde069a6dba27c19b71
techtobit <nurthern2016@gmail.com>
2023-08-30 01:56:47 +0600

    updated home page: dynamic name

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import avatar from "../assets/avatar.jpeg";
 import { Link } from 'react-router-dom';
-import Form from './Form';
-export const Home = ({existingDocument}) => {
+import { useGet } from '../Hooks/useApiFetching';
+export const Home = () => {
     const [btnActive, setBtnActive] = useState(true);
+    const [name, setName] = useState();
+    const { data: getUserName } = useGet('/saveData');
 
-    useEffect( () => {
-    },[] )
+    useEffect(() => {
+        if (getUserName) {
+            setName(getUserName[0]?.name);
+        }
+    }, [getUserName]);
     return (
         <div className='w-screen grid justify-center bg-lightWhite font-poppins  '>
         <div className='lg:w-[850px] md:w[750px] lg:pl-20 pl-10 md:px-[12rem] px-20 pt-2 grid justify-start bg-white border-x-[1px] border-t-[1px] border-borderColor'>
@@ -14,7 +19,7 @@ export const Home = ({existingDocument}) => {
         <div className='profile-head flex  pt-6 pb-4 items-center'>
                 <img className='w-20 rounded-full' src={avatar} />
                 <div className='pl-10'>
-                    <h2 className='font-bold pb-1'>{existingDocument ? existingDocument.name : 'Alex'}</h2>
+                    <h2 className='font-bold pb-1'>{name ? name : 'Alex'}</h2>
                     <h4 className='pb-1 text-sm'>Full Stack Developer</h4>
                     <h4 className='pb-1 text-sm' >Bangladesh</h4>
                 </div>
